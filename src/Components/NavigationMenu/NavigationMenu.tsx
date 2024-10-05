76e071757314b1fd632e9556d6d849422b215c6c
Name <69354246+zVoidra@users.noreply.github.com>
2024-10-05 19:00:10 +0200

    Added "NavigationCard".

--- src/Components/NavigationMenu/NavigationMenu.tsx
+++ src/Components/NavigationMenu/NavigationMenu.tsx
@@ -3,7 +3,13 @@ import './NavigationMenu.css'
 export default function NavigationMenu () {
   return (
     <div className="NavigationMenu">
-      <h1>Navigation Menu</h1>
+      <div className="NavigationCard">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="3em" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z"/>
+        </svg>
+        <div>My Portfolio</div>
+        <button>Expander</button>
+      </div>
     </div>
   );
 }
